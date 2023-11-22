bae26466c1c3c6ccc80cf46552f6384558eac826
marcbenbouik <marc.benbouik1.com>
2023-11-22 22:40:28 +0100

    deploy

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/reset.css';
+import './styles/style.css';
+import Main from './component/main/Main';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProjectStories from './pages/ProjectStories';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/project' element={<ProjectStories />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
