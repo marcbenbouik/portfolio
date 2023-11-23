f16fe0652d73008e5bc8951756f2e37ed631832d
marcbenbouik <marc.benbouik1.com>
2023-11-23 01:59:29 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ import ProjectStories from './pages/stories/ProjectStories';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter />
     <Router>
       <Routes>
         <Route path='/' element={<Main />} />
